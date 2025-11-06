143e575e773d0e7620689d18aae309fc79f807d0
Joe Blue <realbluehead@gmail.com>
2025-11-06 15:16:06 +0100

    Fixed navbar and added google avatar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SupabaseService } from './supabase.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLink],
+  standalone: true,
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLink, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('qmp');
+  protected userAvatar = signal<string | null>(null);
   http = inject(HttpClient);
   supabaseService = inject(SupabaseService);
 
@@ -19,6 +22,12 @@ export class App {
     if (!data.session) {
       await this.supabaseService.signInWithGoogle();
       return;
+    }
+
+    // Obtenir la foto del perfil
+    const { user } = data.session;
+    if (user?.user_metadata?.['avatar_url']) {
+      this.userAvatar.set(user.user_metadata['avatar_url']);
     }
 
     this.http.get('/.netlify/functions/consulta').subscribe((data) => {
