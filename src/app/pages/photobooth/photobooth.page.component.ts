cbb3d2347e5872c2416f8cbac2a44dfde5a2e3e3
defabregas <kangcor@gmail.com>
2025-11-10 11:43:32 +0100

    Fix spelling of photobooth

--- src/app/pages/photobooth/photobooth.page.component.ts
+++ src/app/pages/photobooth/photobooth.page.component.ts
@@ -5,8 +5,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-photobooth',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './photoboth.page.component.html',
-  styleUrls: ['./photoboth.page.component.scss'],
+  templateUrl: './photobooth.page.component.html',
+  styleUrls: ['./photobooth.page.component.scss'],
 })
 export class PhotoBoothPageComponent {
   // Add your component logic here
