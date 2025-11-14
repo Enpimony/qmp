55650d2e2dd6768b3d7744ad3279e764fd722071
Joe Blue <realbluehead@gmail.com>
2025-11-14 16:40:58 +0100

    rename model

--- src/app/pages/photobooth/photobooth.page.component.ts
+++ src/app/pages/photobooth/photobooth.page.component.ts
@@ -31,7 +31,7 @@ export class PhotoBoothPageComponent {
     // 3) Crea l'ImageSegmenter (model per segmentar persones/selfie)
     this.segmenter = await ImageSegmenter.createFromOptions(visionFileset, {
       baseOptions: {
-        modelAssetPath: 'mediapipe.tflite',
+        modelAssetPath: 'selfie_segmentation.tflite',
       },
       runningMode: 'VIDEO', // important per a vídeo en temps real
     });
