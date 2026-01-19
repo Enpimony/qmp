b5b5557aa4189ab4043fb8af12d9caf765c48bed
Joe Blue <realbluehead@gmail.com>
2026-01-19 12:21:32 +0100

    Login page redesign

--- src/app/pages/login.page.component.ts
+++ src/app/pages/login.page.component.ts
@@ -4,7 +4,39 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  template: ` <button class="btn btn-primary w-full" (click)="login()">Login</button> `,
+  template: `
+    <div class="flex items-center justify-center h-screen">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <div class="logo-container">
+            <img src="logo.webp" alt="Kempo Logo" class="logo" />
+          </div>
+          <p class="text-center mb-2">Si us plau, inicia sessió per continuar.</p>
+          <div class="card-actions justify-center">
+            <button class="btn btn-primary" (click)="login()">Entra amb Google</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .h-screen {
+        height: 100vh;
+      }
+
+      .logo-container {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20px;
+      }
+
+      .logo {
+        max-width: 200px;
+        height: auto;
+      }
+    `,
+  ],
 })
 export class LoginPage {
   private authService = inject(AuthService);
